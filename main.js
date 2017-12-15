db9007cbfc6d370eee2e05667564a376ac82d3ad
Krystian Jabłoński <Krystian Jaboski>
2017-12-15 11:16:35 +0100

    fixes

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ class Blockchain{
 
 let SimpleCoin = new Blockchain();
 console.log('Mining block 1...');
-savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+SimpleCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
 
 console.log('Mining block 2...');
-savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+SimpleCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
